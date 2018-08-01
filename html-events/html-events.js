46f47bce656928ba407a48755cbdd303aaa70ee1
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 18:34:03 +0300

    m

--- html-events/html-events.js
+++ html-events/html-events.js
@@ -1,1 +1,26 @@
 // Put all your JavaScript in this file!
+var bg= document.getElementsByTagName('body');
+
+function r(){
+	bg[0].style.backgroundColor= "red";
+}
+function b(){
+	bg[0].style.backgroundColor= "blue";
+}
+function g(){
+	bg[0].style.backgroundColor= "green";
+}
+function myFunction(event){
+	var x = event.which || event.keyCode;
+	document.getElementById("demo").innerHTML = "The Unicode value is: " + x;
+	if(x==114){
+		r()
+	}
+	if(x==103){
+		g()
+	}
+	if(x==98){
+		b()
+	}
+
+}
